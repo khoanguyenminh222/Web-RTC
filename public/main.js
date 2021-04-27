f46df4a4a956a8108938b14227870d3109f8bffe
khoanguyenminh222 <khoanguyenminh222@gmail.com>
2021-04-27 14:51:06 +0700

    first commit

---
+++ public/main.js
@@ -0,0 +1,95 @@
+const socket = io("http://localhost:3000");
+
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+const peers = {}
+
+var peer = new Peer(undefined, {
+    path: "/",
+    host: "/",
+    port: "3001",
+});
+
+let myVideoStream;
+
+var getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+navigator.mediaDevices
+    .getUserMedia({
+        video: true,
+        audio: false, //tắt mic
+    })
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream);
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        socket.on('user-disconnected', userId => {
+            if (peers[userId]) peers[userId].close()
+        })
+
+    });
+
+peer.on("call", function(call) {
+    getUserMedia({ video: true, audio: true },
+        function(stream) {
+            call.answer(stream); // Answer the call with an A/V stream.
+            const video = document.createElement("video");
+            call.on("stream", function(remoteStream) {
+                addVideoStream(video, remoteStream);
+            });
+        },
+        function(err) {
+            console.log("Failed to get local stream", err);
+        }
+    );
+});
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id);
+});
+
+// CHAT
+
+const connectToNewUser = (userId, streams) => {
+    var call = peer.call(userId, streams);
+    var video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        console.log(userVideoStream);
+        addVideoStream(video, userVideoStream);
+    });
+    peers[userId] = call
+};
+
+const addVideoStream = (videoEl, stream) => {
+    videoEl.srcObject = stream;
+    videoEl.addEventListener("loadedmetadata", () => {
+        videoEl.play();
+    });
+
+    videoGrid.append(videoEl);
+    let totalUsers = document.getElementsByTagName("video").length;
+    if (totalUsers > 1) {
+        for (let index = 0; index < totalUsers; index++) {
+            document.getElementsByTagName("video")[index].style.width =
+                100 / totalUsers + "%";
+        }
+    }
+};
